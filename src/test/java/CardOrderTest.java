175c50a9d91e302824bbcfbf5d3ba95646eae6b8
Alla <lialia13111989@mail.ru>
2023-04-06 21:40:25 +0300

    new commit

--- src/test/java/CardOrderTest.java
+++ src/test/java/CardOrderTest.java
@@ -32,27 +32,27 @@ public class CardOrderTest {
 
     @Test
     void shouldTestCSS() {
-        driver.get("http://localhost:9999");
+        driver.get("http://localhost:7777");
         driver.findElement(By.cssSelector("span[data-test-id=name] input")).sendKeys("Иван Иванов-Иванов");
         driver.findElement(By.cssSelector("span[data-test-id=phone] input")).sendKeys("+79999999999");
         driver.findElement(By.cssSelector("[data-test-id=agreement]")).click();
         driver.findElement(By.tagName("button")).click();
         String expected = "Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.";
-        String actual = driver.findElement(By.className("paragraph")).getText().trim();
+        String actual = driver.findElement(By.className("paragraph_theme_alfa-on-white")).getText().trim();
         assertEquals(expected, actual);
 
     }
 
     @Test
     void shouldTestSelenium() {
-        driver.get("http://localhost:9999");
+        driver.get("http://localhost:7777");
         List<WebElement> inputs = driver.findElements(By.tagName("input"));
         inputs.get(0).sendKeys("Иван Иванов-Иванов");
         inputs.get(1).sendKeys("+79999999999");
         driver.findElement(By.className("checkbox__box")).click();
         driver.findElement(By.tagName("button")).click();
         String expected = "Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.";
-        String actual = driver.findElement(By.className("paragraph")).getText().trim();
+        String actual = driver.findElement(By.className("paragraph_theme_alfa-on-white")).getText().trim();
         assertEquals(expected, actual);
 
     }
