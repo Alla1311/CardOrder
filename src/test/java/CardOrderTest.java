33fa1914973f0052540ded750dc0f1e1bf3e80aa
Alla <lialia13111989@mail.ru>
2023-04-06 23:02:46 +0300

    new commit

--- src/test/java/CardOrderTest.java
+++ src/test/java/CardOrderTest.java
@@ -4,11 +4,8 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-
-import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -16,11 +13,11 @@ public class CardOrderTest {
     private WebDriver driver;
 
     @BeforeEach
-    void setUpAll() {
+    void setUp() {
         WebDriverManager.chromedriver().setup();
-        ChromeOptions chromeOption = new ChromeOptions();
-        chromeOption.addArguments("--headless");
-        driver = new ChromeDriver(chromeOption);
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless");
+        driver = new ChromeDriver(options);
     }
 
 
@@ -38,23 +35,10 @@ public class CardOrderTest {
         driver.findElement(By.cssSelector("[data-test-id=agreement]")).click();
         driver.findElement(By.tagName("button")).click();
         String expected = "Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.";
-        String actual = driver.findElement(By.className("paragraph_theme_alfa-on-white")).getText().trim();
+        String actual = driver.findElement(By.cssSelector("[data-test-id=order-success]")).getText().trim();
         assertEquals(expected, actual);
 
     }
 
-    @Test
-    void shouldTestSelenium() {
-        driver.get("http://localhost:7777");
-        List<WebElement> inputs = driver.findElements(By.tagName("input"));
-        inputs.get(0).sendKeys("Иван Иванов-Иванов");
-        inputs.get(1).sendKeys("+79999999999");
-        driver.findElement(By.className("checkbox__box")).click();
-        driver.findElement(By.tagName("button")).click();
-        String expected = "Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.";
-        String actual = driver.findElement(By.className("paragraph_theme_alfa-on-white")).getText().trim();
-        assertEquals(expected, actual);
-
-    }
 
 }
